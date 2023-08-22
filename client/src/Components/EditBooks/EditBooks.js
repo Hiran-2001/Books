8b88883bcb226683d5e65f497b906108dc090e62
Hiran Raj <hirankvlr@gmail.com>
2023-08-22 21:25:18 +0530

    almost done

--- client/src/Components/EditBooks/EditBooks.js
+++ client/src/Components/EditBooks/EditBooks.js
@@ -1,7 +1,6 @@
 import React,{useState,useEffect} from "react";
 import {useParams, useNavigate, Link} from "react-router-dom";
 import  axios  from "axios";
-import "./EditBooks.css"
 function EditBooks() {
     const navigate = useNavigate()
 
